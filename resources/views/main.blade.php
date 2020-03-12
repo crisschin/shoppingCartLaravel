42ccb2cee068988aa112fd89465b575bbd0014a7
crisschin <criss_1231@hotmail.com>
2020-03-12 14:29:32 +0800

    completed

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -140,7 +140,7 @@ input[type=text], select, textarea {
             <form class="form-inline my-2 my-lg-0" method="post" action="{{ route('search.product') }}">
               {{csrf_field() }} 
               <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="searchProduct"> 
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="margin:10px">Search</button>
             </form>
 
             <div class="flex-center position-ref full-height">
