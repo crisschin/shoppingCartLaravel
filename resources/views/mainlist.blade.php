e04f6fcda7a00654a712c3cd7d86f5640a13cde0
crisschin <criss_1231@hotmail.com>
2020-03-12 14:25:08 +0800

    completed

--- resources/views/mainlist.blade.php
+++ resources/views/mainlist.blade.php
@@ -8,7 +8,8 @@
                                     <h5 class="card-title">{{$product->name}}</h5>
                                     <a href="{{ route('product.detail', ['id' =>$product->id]) }}"><img src="{{ asset('pic/') }}/{{$product->image}}" alt="" class="img-fluid"></a>
                                     <div class="card-heading">RM {{$product->price}}</div>
-                                    <button style="float:right" class="btn btn-danger btn-xs">Add to Cart</button>
+                                    <a href="{{ route('product.detail', ['id' => $product->id])}}">
+                                    <button style="float:right" class="btn btn-success btn-xs">Details</button></a>
                                 </div>
                             </div>
                         </div>
